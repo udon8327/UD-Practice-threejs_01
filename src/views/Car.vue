71e90c40d2aca825fe826ddddeb6cc9ecd2a3ecd
Bohan <bohan.huang@coolbe.com.tw>
2024-08-08 10:12:49 +0800

    save

--- src/views/Car.vue
+++ src/views/Car.vue
@@ -20,6 +20,14 @@ onMounted(() => {
   // 創建場景
   const scene = new THREE.Scene();
 
+  // 創建坐標線
+  const axesHelper = new THREE.AxesHelper(150);
+  scene.add(axesHelper);
+
+  // 創建網格地面
+  const gridHelper = new THREE.GridHelper(20, 20);
+  scene.add(gridHelper);
+
   // 創建相機
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   camera.position.set(0, 1, 5);
@@ -29,7 +37,7 @@ onMounted(() => {
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  // 添加光源
+  // 添加光源
   const light = new THREE.DirectionalLight(0xffffff, 1);
   light.position.set(5, 5, 5).normalize();
   scene.add(light);
@@ -44,14 +52,17 @@ onMounted(() => {
 
   // 創建地面
   const geometry = new THREE.BoxGeometry(1000, 0.001, 1000)
-  const material = new THREE.MeshBasicMaterial({color: 0x3b3b3b})
+  const material = new THREE.MeshBasicMaterial({color: 0x2b2b2b})
   const cube = new THREE.Mesh(geometry, material);
+  cube.position.set(0, -0.1, 0);
   scene.add(cube);
 
   // 加載glTF模型
   const loader = new GLTFLoader();
   loader.load('/3d-models/lamborghini/scene.gltf', function (gltf) {
-    scene.add(gltf.scene);
+    const car = gltf.scene;
+    car.position.set(0, 0, 0);
+    scene.add(car);
   }, undefined, function (error) {
     console.error(error);
   });
